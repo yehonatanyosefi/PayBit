fafa37c4e26bc84e6014a574a5de5d37e2f7d296
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-05-01 18:22:39 +0300

    Auto-generated commit [ci skip]

--- src/services/friend.resolver.ts
+++
@@ -1,8 +0,0 @@
-import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot } from '@angular/router';
-import { FriendService } from './friend.service';
-
-export function friendResolver(route: ActivatedRouteSnapshot) {
-  const id = route.params['id'];
-  return inject(FriendService).getFriendById(id).pipe();
-}
